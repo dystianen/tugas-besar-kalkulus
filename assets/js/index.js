6829b609153254d321d6afd109e2057718bc9371
Dystian En <distian.enyus@gmail.com>
2022-12-10 20:11:30 +0700

    default value

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,33 @@
 const resultSuccess = document.getElementById('result-success');
 const resultError = document.getElementById('result-error');
 
+function onChange(type) {
+  if (type === 'A') {
+    const A = document.getElementById("nilaiA").value;
+    document.getElementById('A').innerHTML = A;
+  } else if (type === 'B') {
+    const B = document.getElementById("nilaiB").value;
+    document.getElementById('B').innerHTML = B;
+  } else {
+    const C = document.getElementById("nilaiC").value;
+    document.getElementById('C').innerHTML = C;
+  }
+}
+
 function onSubmit() {
   const A = Number(document.getElementById('nilaiA').value);
   const B = Number(document.getElementById('nilaiB').value);
   const C = Number(document.getElementById('nilaiC').value);
 
   const adrat = Math.pow(B, 2) - 4 * A * C;
+  const mutlak = Math.abs(adrat);
 
   let x1 = 0;
   let x2 = 0;
 
   if (adrat > 0) {
-    x1 = (-B + Math.sqrt(adrat)) / (2 * A);
-    x2 = (-B - Math.sqrt(adrat)) / (2 * A);
+    x1 = (-B + Math.sqrt(mutlak)) / (2 * A);
+    x2 = (-B - Math.sqrt(mutlak)) / (2 * A);
 
     document.getElementById('x1').innerHTML = x1.toFixed(2);
     document.getElementById('x2').innerHTML = x2.toFixed(2);
@@ -25,6 +39,9 @@ function onSubmit() {
 }
 
 function onReset() {
+  document.getElementById("A").innerHTML = 'A';
+  document.getElementById("B").innerHTML = 'B';
+  document.getElementById("C").innerHTML = 'C';
   resultSuccess.style.display = 'none';
   resultError.style.display = 'none';
   document.getElementById('x1').value = 0;
