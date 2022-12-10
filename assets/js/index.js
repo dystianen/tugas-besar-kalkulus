ec4e5d7831bc190d662dbed4695a0111b3f2ea94
Dystian En <distian.enyus@gmail.com>
2022-12-10 20:25:46 +0700

    fix: value

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,13 @@ const resultError = document.getElementById('result-error');
 
 function onChange(type) {
   if (type === 'A') {
-    const A = document.getElementById("nilaiA").value;
+    const A = document.getElementById('nilaiA').value;
     document.getElementById('A').innerHTML = A;
   } else if (type === 'B') {
-    const B = document.getElementById("nilaiB").value;
+    const B = document.getElementById('nilaiB').value;
     document.getElementById('B').innerHTML = B;
   } else {
-    const C = document.getElementById("nilaiC").value;
+    const C = document.getElementById('nilaiC').value;
     document.getElementById('C').innerHTML = C;
   }
 }
@@ -25,24 +25,31 @@ function onSubmit() {
   let x1 = 0;
   let x2 = 0;
 
+  const denominator = 2 * A;
+  const squareroot = Math.sqrt(mutlak);
+
   if (adrat > 0) {
-    x1 = (-B + Math.sqrt(mutlak)) / (2 * A);
-    x2 = (-B - Math.sqrt(mutlak)) / (2 * A);
+    x1 = (-B + squareroot) / denominator;
+    x2 = (-B - squareroot) / denominator;
 
     document.getElementById('x1').innerHTML = x1.toFixed(2);
     document.getElementById('x2').innerHTML = x2.toFixed(2);
     resultSuccess.style.display = 'block';
     resultError.style.display = 'none';
   } else {
-    resultError.style.display = 'block';
-    resultSuccess.style.display = 'none';
+    resultSuccess.style.display = 'block';
+    let x_second = (-1 * B) / denominator;
+    let positive_num_sec = (squareroot / denominator).toFixed(2) + 'i';
+    let negative_num_sec = ((-1 * squareroot) / denominator).toFixed(2) + 'i';
+    document.getElementById('x1').innerHTML = x_second.toFixed(2) + ' + ' + positive_num_sec;
+    document.getElementById('x2').innerHTML = x_second.toFixed(2) + ' ' + negative_num_sec;
   }
 }
 
 function onReset() {
-  document.getElementById("A").innerHTML = 'A';
-  document.getElementById("B").innerHTML = 'B';
-  document.getElementById("C").innerHTML = 'C';
+  document.getElementById('A').innerHTML = 'A';
+  document.getElementById('B').innerHTML = 'B';
+  document.getElementById('C').innerHTML = 'C';
   resultSuccess.style.display = 'none';
   resultError.style.display = 'none';
   document.getElementById('x1').value = 0;
